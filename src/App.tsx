a189903447e6b23438e444ae0044e6cfd8a31779
Iris Adina Morar <morariris@gmail.com>
2026-03-03 15:34:55 +0200

    reparing the app logic for a new implementation - replace useReducer() with useState()

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
 import "./App.css";
 
-export default function App() {}
+export default function App() {
+  return (
+    <main>
+      <div></div>
+      <section className="game-container">
+        <div className="player_name">P1</div>
+        <section></section>
+        <div className="player_name">P2</div>
+      </section>
+    </main>
+  );
+}
--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -1,3 +1,5 @@
+import type { BoardState } from "./initialState";
+
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -9,17 +11,29 @@ const WINNING_COMBINATIONS = [
   [2, 4, 6],
 ];
 
-const getPlayer1Moves = (state: { moves: number[] }) => {
-  return state.moves.filter((_, index) => index % 2 === 0);
+const getPlayer1Moves = (state: BoardState) => {
+  const player1MoveIndices: number[] = [];
+  for (let i = 0; i < 9; i++) {
+    if (state.moves[i] === "X") {
+      player1MoveIndices.push(i);
+    }
+  }
+  return player1MoveIndices;
 };
 
-const getPlayer2Moves = (state: { moves: number[] }) => {
-  return state.moves.filter((_, index) => index % 2 !== 0);
+const getPlayer2Moves = (state: BoardState) => {
+  const player2MoveIndices: number[] = [];
+  for (let i = 0; i < 9; i++) {
+    if (state.moves[i] === "0") {
+      player2MoveIndices.push(i);
+    }
+  }
+  return player2MoveIndices;
 };
 
 const movesIncludeCombination = (
   playerMoves: number[],
-  combination: [number, number, number],
+  combination: number[],
 ) => {
   for (const combinationPosition of combination) {
     if (!playerMoves.includes(combinationPosition)) {
@@ -27,4 +41,45 @@ const movesIncludeCombination = (
     }
   }
   return true;
+};
+
+export const hasPlayer1Won = (state: BoardState) => {
+  for (const winningCombination of WINNING_COMBINATIONS) {
+    if (movesIncludeCombination(getPlayer1Moves(state), winningCombination)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const hasPlayer2Won = (state: BoardState) => {
+  for (const winningCombination of WINNING_COMBINATIONS) {
+    if (movesIncludeCombination(getPlayer2Moves(state), winningCombination)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const getWinningCombination = (state: BoardState) => {
+  for (const winningCombination of WINNING_COMBINATIONS) {
+    if (
+      movesIncludeCombination(getPlayer1Moves(state), winningCombination) ||
+      movesIncludeCombination(getPlayer2Moves(state), winningCombination)
+    ) {
+      return winningCombination;
+    }
+  }
+};
+
+export const hasGameBeenWon = (state: BoardState) => {
+  return hasPlayer1Won(state) || hasPlayer2Won(state);
+};
+
+export const isDraw = (state: BoardState) => {
+  return state.moves.length === 9 && !hasGameBeenWon(state);
+};
+
+export const isGameOver = (state: BoardState) => {
+  return hasGameBeenWon(state) || isDraw(state);
 };
